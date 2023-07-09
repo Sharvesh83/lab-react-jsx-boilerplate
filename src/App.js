b6f6fa757e5e0a81aa999d7a0b999e46510937f1
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-09 23:27:59 +0530

    Second Lab

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import './App.css';
-import elephant from "./images/elephant.jpeg";
-
-const imageData = () => {
-  let data = [
-    {
-      id: 1,
-      img: elephant
-    },
-    {
-      id: 2,
-      img: elephant
-    },
-    {
-      id: 3,
-      img: elephant
-    },
-    {
-      id: 4,
-      img: elephant
-    }
-  ];
-  return data;
-};
+import { imageData } from "./components/DataComponent";
 
 function App() {
-  const data = imageData();
-
   return (
     <div className="app">
       <h1>Kalvium Gallery</h1>
       <div className="image-container">
-        {data.map((item) => (
+        {imageData.map((item) => (
           <img key={item.id} src={item.img} alt={`${item.id}`} />
         ))}
       </div>
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,38 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import elephant from "./images/elephant.jpeg";
+import { imageData } from "./components/DataComponent";
+
 
 export default class AppClass extends Component {
-  imageData = () => {
-    let data = [
-      {
-        id: 1,
-        img: elephant
-      },
-      {
-        id: 2,
-        img: elephant
-      },
-      {
-        id: 3,
-        img: elephant
-      },
-      {
-        id: 4,
-        img: elephant
-      }
-    ];
-    return data;
-  };
-
   render() {
-    const data = this.imageData();
-
     return (
       <div className="app">
         <h1>Kalvium Gallery</h1>
         <div className="image-container">
-          {data.map((item) => (
+          {imageData.map((item) => (
             <img key={item.id} src={item.img} alt={`${item.id}`} />
           ))}
         </div>
