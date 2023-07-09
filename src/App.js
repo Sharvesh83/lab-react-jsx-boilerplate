c7a29805b799b7e41ca5412798eb88888cb075bc
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-09 23:12:53 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
+import React from 'react';
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant
     },
     {
-      id:4,
-      img:elephant
+      id: 4,
+      img: elephant
     }
-  ]
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  const data = imageData();
+
+  return (
+    <div className="app">
+      <h1>Kalvium Gallery</h1>
+      <div className="image-container">
+        {data.map((item) => (
+          <img key={item.id} src={item.img} alt={`${item.id}`} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
